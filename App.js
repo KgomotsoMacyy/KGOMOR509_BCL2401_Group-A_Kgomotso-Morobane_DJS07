e592e2cc0d70c85803bf08b2d433972cebfaa7ac
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-05-30 15:02:57 +0200

    Intialized Repository

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./components/Header"
+import Mem from "./components/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header>
+                
+            </Header>
+        </div>
+    )
+}
