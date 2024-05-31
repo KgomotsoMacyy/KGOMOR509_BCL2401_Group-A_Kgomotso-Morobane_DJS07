eb3bac3d01492176c994063495bf18081456cb78
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-05-31 08:40:17 +0200

    used the async function inside the useEffect

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Header from "./components/Header"
-import Mem from "./components/Meme"
-
+import Meme from "./components/Meme"
+/**
+ * Challenge: 
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
 export default function App() {
     return (
         <div>
-            <Header>
-                
-            </Header>
+            <Header />
+            <Meme />
         </div>
     )
 }
